3f961e447134618c78fbbc3953f09c1e6296e088
chao <chenrongchao@lbesec.com>
2020-06-03 00:22:20 +0800

    实现简单液体效果

--- app/src/main/java/cc/rome753/surfacepaint/box2d/PhysicsLayout.java
+++ app/src/main/java/cc/rome753/surfacepaint/box2d/PhysicsLayout.java
@@ -2,6 +2,8 @@ package cc.rome753.surfacepaint.box2d;
 
 import android.content.Context;
 import android.graphics.Canvas;
+import android.graphics.Color;
+import android.graphics.Paint;
 import android.util.AttributeSet;
 import android.util.Log;
 import android.view.MotionEvent;
@@ -26,6 +28,8 @@ import org.jbox2d.dynamics.World;
 import org.jbox2d.dynamics.joints.JointType;
 import org.jbox2d.dynamics.joints.MouseJoint;
 import org.jbox2d.dynamics.joints.MouseJointDef;
+import org.jbox2d.particle.ParticleGroupDef;
+import org.jbox2d.particle.ParticleType;
 
 import java.util.Random;
 
@@ -41,6 +45,7 @@ public class PhysicsLayout extends FrameLayout {
     private Body groundBody;
 
     private Vec2 hitPoint = new Vec2();
+    private Paint paint = new Paint();
 
     public PhysicsLayout(@NonNull Context context) {
         this(context, null);
@@ -53,12 +58,16 @@ public class PhysicsLayout extends FrameLayout {
     public PhysicsLayout(@NonNull Context context, @Nullable AttributeSet attrs, int defStyleAttr) {
         super(context, attrs, defStyleAttr);
         setWillNotDraw(false);
+        paint.setColor(Color.RED);
+        paint.setStrokeWidth(5);
+        paint.setStyle(Paint.Style.STROKE);
     }
 
     @Override
     protected void onDraw(Canvas canvas) {
         if (world == null) {
             createWorld();
+            createLiquid();
         }
         for (int i = 0; i < getChildCount(); i++) {
             View view = getChildAt(i);
@@ -68,7 +77,10 @@ public class PhysicsLayout extends FrameLayout {
             }
         }
 
-        world.step(dt, 10, 10);
+        long time = System.currentTimeMillis();
+        world.step(dt, 1, 1);
+        long time1 = System.currentTimeMillis();
+        Log.d("chao", "step time " + (time1 - time));
         for (int i = 0; i < getChildCount(); i++) {
             View view = getChildAt(i);
             Body body = (Body) view.getTag();
@@ -77,7 +89,37 @@ public class PhysicsLayout extends FrameLayout {
             }
         }
 
+        // draw particles
+        Vec2[] particlePositionBuffer = world.getParticlePositionBuffer();
+        for (Vec2 vec2 : particlePositionBuffer) {
+            float x = meter2Pixel(vec2.x);
+            float y = meter2Pixel(vec2.y);
+            canvas.drawCircle(x, y, 15, paint);
+        }
+        long time2 = System.currentTimeMillis();
+        Log.d("chao", "draw time " + (time2 - time1));
+
         invalidate();
+    }
+
+
+    private void createLiquid() {
+//        world.setParticleRadius(0.15f);
+        world.setParticleRadius(0.5f);
+        world.setParticleDamping(1f);
+
+        ParticleGroupDef pd = new ParticleGroupDef();
+//        pd.flags = ParticleType.b2_elasticParticle;
+        pd.flags = ParticleType.b2_waterParticle;
+        pd.strength = 2f;
+
+        PolygonShape shape = new PolygonShape();
+        shape.setAsBox(8f, 8f, new Vec2(20f, 20f), 0f);
+
+        pd.shape = shape;
+        world.createParticleGroup(pd);
+
+        Log.d("chao", "create particles " + world.getParticleCount());
     }
 
     QueryCallback queryCallback = new QueryCallback() {
@@ -131,18 +173,6 @@ public class PhysicsLayout extends FrameLayout {
         gravity.y = y;
         if (world != null) {
             world.setGravity(gravity);
-        }
-    }
-
-    public void setForce(float x, float y) {
-        for (int i = 0; i < getChildCount(); i++) {
-            Vec2 impulse = new Vec2(x, y);
-            View view = getChildAt(i);
-            Body body = (Body) view.getTag();
-            if (body != null) {
-                body.applyForce(impulse, body.getPosition());
-//                body.applyLinearImpulse(impulse, body.getPosition());
-            }
         }
     }
 
