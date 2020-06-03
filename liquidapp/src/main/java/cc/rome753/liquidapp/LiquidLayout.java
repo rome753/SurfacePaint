eab4bf3a9e4b49699ad14f55a5f5add6ecf6f3b7
chao <chenrongchao@lbesec.com>
2020-06-03 18:57:55 +0800

    加入swig编译的liquidfun模块

---
+++ liquidapp/src/main/java/cc/rome753/liquidapp/LiquidLayout.java
@@ -0,0 +1,175 @@
+package cc.rome753.liquidapp;
+
+import android.content.Context;
+import android.graphics.Canvas;
+import android.graphics.Color;
+import android.graphics.Paint;
+import android.support.annotation.NonNull;
+import android.support.annotation.Nullable;
+import android.util.AttributeSet;
+import android.util.Log;
+import android.widget.FrameLayout;
+
+import com.google.fpl.liquidfun.Body;
+import com.google.fpl.liquidfun.BodyDef;
+import com.google.fpl.liquidfun.BodyType;
+import com.google.fpl.liquidfun.FixtureDef;
+import com.google.fpl.liquidfun.ParticleFlag;
+import com.google.fpl.liquidfun.ParticleGroup;
+import com.google.fpl.liquidfun.ParticleGroupDef;
+import com.google.fpl.liquidfun.ParticleSystem;
+import com.google.fpl.liquidfun.ParticleSystemDef;
+import com.google.fpl.liquidfun.PolygonShape;
+import com.google.fpl.liquidfun.Vec2;
+import com.google.fpl.liquidfun.World;
+
+import java.util.Random;
+
+public class LiquidLayout extends FrameLayout {
+
+    private static final float GRAVITY = 10f;
+    private World world;
+    private ParticleSystem particleSystem;
+    private Vec2 gravity = new Vec2(0, GRAVITY);
+    private int ratio = 50;
+    private float dt = 1f / 60f;
+    private Random random = new Random();
+//    private MouseJoint mouseJoint;
+    private Body groundBody;
+
+    private Vec2 hitPoint = new Vec2();
+    private Paint paint = new Paint();
+
+    public LiquidLayout(@NonNull Context context) {
+        this(context, null);
+    }
+
+    public LiquidLayout(@NonNull Context context, @Nullable AttributeSet attrs) {
+        this(context, attrs, 0);
+    }
+
+    public LiquidLayout(@NonNull Context context, @Nullable AttributeSet attrs, int defStyleAttr) {
+        super(context, attrs, defStyleAttr);
+        setWillNotDraw(false);
+        paint.setColor(Color.RED);
+        paint.setStrokeWidth(5);
+        paint.setStyle(Paint.Style.STROKE);
+    }
+
+    @Override
+    protected void onDraw(Canvas canvas) {
+        if (world == null) {
+            createWorld();
+            createLiquid();
+        }
+
+//        long time = System.currentTimeMillis();
+//        world.step(dt, 1, 1, 1);
+//        long time1 = System.currentTimeMillis();
+//        Log.d("chao", "step time " + (time1 - time));
+//
+//        // draw particles
+//        for (int i = 0; i < particleSystem.getParticleCount(); i++) {
+//            float x = meter2Pixel(particleSystem.getParticlePositionX(i));
+//            float y = meter2Pixel(particleSystem.getParticlePositionY(i));
+//            canvas.drawCircle(x, y, 15, paint);
+//        }
+//
+//        long time2 = System.currentTimeMillis();
+//        Log.d("chao", "draw time " + (time2 - time1));
+
+        invalidate();
+    }
+
+
+    private void createLiquid() {
+        ParticleSystemDef psd = new ParticleSystemDef();
+        psd.setDensity(1.2f);
+        psd.setGravityScale(0.4f);
+        psd.setRadius(0.5f);
+        psd.setRepulsiveStrength(0.5f);
+        particleSystem = world.createParticleSystem(psd);
+        particleSystem.setMaxParticleCount(1000);
+
+        PolygonShape shape = new PolygonShape();
+        shape.setAsBox(8f, 8f, 10f, 10f, 0f);
+
+        ParticleGroupDef pd = new ParticleGroupDef();
+        pd.setFlags(0);
+        pd.setGroupFlags(0);
+        pd.setLinearVelocity(new Vec2(0,0));
+        pd.setShape(shape);
+
+        // signal 11 (SIGSEGV), code 1 (SEGV_MAPERR), fault addr 0x25
+        // Cause: null pointer dereference
+//        particleSystem.createParticleGroup(pd);
+
+        psd.delete();
+        shape.delete();
+        pd.delete();
+
+        Log.d("chao", "create particles " + particleSystem.getParticleCount());
+    }
+
+    public void setGravity(float x, float y) {
+        gravity.set(x, y);
+        if (world != null) {
+            world.setGravity(x, y);
+        }
+    }
+
+    private void createWorld() {
+        float w = pixel2Meter(getWidth()), h = pixel2Meter(getHeight());
+        Log.d("chao", "createWorld " + w + "," + h);
+        float wall = 1f;
+        world = new World(gravity.getX(), gravity.getY());
+
+        PolygonShape wallShape = new PolygonShape();
+        wallShape.setAsBox(w, wall);
+
+        FixtureDef fixtureDef = new FixtureDef();
+        fixtureDef.setShape(wallShape);
+
+        BodyDef bodyDef = new BodyDef();
+        bodyDef.setType(BodyType.staticBody);
+
+        bodyDef.setPosition(0, -wall);
+        Body top = world.createBody(bodyDef);
+        top.createFixture(fixtureDef);
+
+        bodyDef.setPosition(0, h + wall);
+        groundBody = world.createBody(bodyDef);
+        groundBody.createFixture(fixtureDef);
+
+        wallShape.setAsBox(wall, h);
+
+        bodyDef.setPosition(-wall, 0);
+        Body left = world.createBody(bodyDef);
+        left.createFixture(fixtureDef);
+
+        bodyDef.setPosition(w + wall, 0);
+        Body right = world.createBody(bodyDef);
+        right.createFixture(fixtureDef);
+
+        fixtureDef.delete();
+        wallShape.delete();
+        bodyDef.delete();
+    }
+
+    private float pixel2Meter(float pixel) {
+        return pixel / ratio;
+    }
+
+    private float meter2Pixel(float meter) {
+        return meter * ratio;
+    }
+
+    private float radiansToDegrees(float radians) {
+        return radians / 3.14f * 180f;
+    }
+
+    private float degreesToRadians(float degrees) {
+        return (degrees / 180f) * 3.14f;
+    }
+
+}
---
+++ liquidapp/src/main/java/cc/rome753/liquidapp/MainActivity.java
@@ -0,0 +1,58 @@
+package cc.rome753.liquidapp;
+
+import android.hardware.Sensor;
+import android.hardware.SensorEvent;
+import android.hardware.SensorEventListener;
+import android.hardware.SensorManager;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    static {
+        System.loadLibrary("liquidfun");
+        System.loadLibrary("liquidfun_jni");
+    }
+
+    private LiquidLayout physicsLayout;
+    private SensorManager sensorManager;
+
+    private SensorEventListener sensorEventListener = new SensorEventListener() {
+        @Override
+        public void onSensorChanged(SensorEvent event) {
+            if (event.sensor.getType() == Sensor.TYPE_ACCELEROMETER) {
+                float x = event.values[0] * 10;
+                float y = event.values[1] * 10;
+                physicsLayout.setGravity(-x, y);
+            }
+        }
+
+        @Override
+        public void onAccuracyChanged(Sensor sensor, int accuracy) {
+
+        }
+    };
+
+    @Override
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        physicsLayout = findViewById(R.id.liq);
+        sensorManager = (SensorManager) getSystemService(SENSOR_SERVICE);
+    }
+
+    @Override
+    protected void onPause() {
+        super.onPause();
+        sensorManager.unregisterListener(sensorEventListener);
+    }
+
+    @Override
+    protected void onResume() {
+        super.onResume();
+        sensorManager.registerListener(sensorEventListener,
+                sensorManager.getDefaultSensor(Sensor.TYPE_ACCELEROMETER),
+                SensorManager.SENSOR_DELAY_UI);
+
+    }
+}
