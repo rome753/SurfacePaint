130720154f6082bb035307e4075a631959dd477f
rome753 <rome753@163.com>
2022-03-10 15:34:57 +0800

    liquidapp彩色浪花

--- liquidapp/src/main/assets/shader_base_f.glsl
+++ liquidapp/src/main/assets/shader_base_f.glsl
@@ -1,7 +1,10 @@
 #version 300 es
 precision mediump float;
+
+in vec2 vPos;
+
 out vec4 fragColor;
 
 void main() {
-     fragColor = vec4(255.0f, 0.0f, 0.0f, 1.0f);
+     fragColor = vec4((vPos + 1.0f) * 0.5f, 0.5f, 1.0f);
 }
--- liquidapp/src/main/assets/shader_base_v.glsl
+++ liquidapp/src/main/assets/shader_base_v.glsl
@@ -1,7 +1,10 @@
 #version 300 es
 layout (location = 0) in vec2 vPosition;
 
+out vec2 vPos;
+
 void main() {
+     vPos = vPosition / 10.0f;
      gl_PointSize = 20.0f;
-     gl_Position  = vec4(vPosition / 10.0f, 0.0f, 1.0f);
+     gl_Position  = vec4(vPos, 0.0f, 1.0f);
 }
