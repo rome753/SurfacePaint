0eed8539b9ae7c088bd787c8f51f4225bae31180
rome753 <rome753@163.com>
2022-03-18 16:36:47 +0800

    opengles3 group3d在2d基础上稍微改改就行了！

--- opengles3/src/main/java/cc/rome753/opengles3/shader/Group3DRender.java
+++ opengles3/src/main/java/cc/rome753/opengles3/shader/Group3DRender.java
@@ -10,10 +10,12 @@ import java.util.HashSet;
 import java.util.Random;
 
 import static android.opengl.GLES20.GL_DEPTH_BUFFER_BIT;
+import static android.opengl.GLES20.GL_DEPTH_TEST;
 import static android.opengl.GLES20.GL_POINTS;
 import static android.opengl.GLES20.GL_STREAM_DRAW;
 import static android.opengl.GLES20.glBufferSubData;
 import static android.opengl.GLES20.glDrawArrays;
+import static android.opengl.GLES20.glEnable;
 import static android.opengl.GLES20.glGetProgramiv;
 import static android.opengl.GLES20.glGetUniformLocation;
 import static android.opengl.GLES20.glUniform2fv;
@@ -52,8 +54,8 @@ public class Group3DRender extends BaseRender {
     static int MAX_COUNT = 200;
     static int BOUND = 1024;
 
-    float[] pos = new float[MAX_COUNT * 2];
-    float[] vel = new float[MAX_COUNT * 2];
+    float[] pos = new float[MAX_COUNT * 3];
+    float[] vel = new float[MAX_COUNT * 3];
 
     ByteBuffer posBuffer;
 
@@ -65,7 +67,7 @@ public class Group3DRender extends BaseRender {
     int visiDis = 300 * 300;
     int closeDis = 120 * 120;
 
-    static int PARTS = 16; // 8 * 8
+    static int PARTS = 16; // 16 * 16 * 16
     static int partW = BOUND / PARTS;
 
     // PART index -> Part
@@ -73,12 +75,9 @@ public class Group3DRender extends BaseRender {
 
     float[] tempPos = new float[pos.length];
 
-    int[] neighborsDir = {
-            -1, -1, 0, -1, 1, -1,
-            -1,  0, 0,  0, 1,  0,
-            -1,  1, 0,  1, 1,  1,
-    };
-    int[] neighbors = new int[pos.length / 2 + 1];
+    // 空间中27个方块的三维坐标偏移
+    int[] neighborsDir = {-1, -1, -1, -1, -1, 0, -1, -1, 1, -1, 0, -1, -1, 0, 0, -1, 0, 1, -1, 1, -1, -1, 1, 0, -1, 1, 1, 0, -1, -1, 0, -1, 0, 0, -1, 1, 0, 0, -1, 0, 0, 0, 0, 0, 1, 0, 1, -1, 0, 1, 0, 0, 1, 1, 1, -1, -1, 1, -1, 0, 1, -1, 1, 1, 0, -1, 1, 0, 0, 1, 0, 1, 1, 1, -1, 1, 1, 0, 1, 1, 1};
+    int[] neighbors = new int[pos.length / 3 + 1];
 
     @Override
     public void onSurfaceCreated(GL10 gl, EGLConfig config) {
@@ -90,7 +89,7 @@ public class Group3DRender extends BaseRender {
 
 //        //分配内存空间,每个浮点型占4字节空间
         posBuffer = ByteBuffer
-                .allocateDirect(2 * 4 * MAX_COUNT)
+                .allocateDirect(3 * 4 * MAX_COUNT)
                 .order(ByteOrder.nativeOrder());
 
         vao = new int[1];
@@ -101,10 +100,10 @@ public class Group3DRender extends BaseRender {
         glGenBuffers(1, vbo, 0);
         glBindBuffer(GL_ARRAY_BUFFER, vbo[0]);
 //        glBufferData(GL_ARRAY_BUFFER, vertices.length * 4, vertexBuffer, GL_STATIC_DRAW);
-        glBufferData(GL_ARRAY_BUFFER, MAX_COUNT * 4 * 2, posBuffer, GL_STREAM_DRAW);
+        glBufferData(GL_ARRAY_BUFFER, MAX_COUNT * 4 * 3, posBuffer, GL_STREAM_DRAW);
 
         // Load the vertex data
-        glVertexAttribPointer(0, 2, GL_FLOAT, false, 2 * 4, 0);
+        glVertexAttribPointer(0, 3, GL_FLOAT, false, 3 * 4, 0);
         glEnableVertexAttribArray(0);
 
         glBindBuffer(GL_ARRAY_BUFFER, 0);
@@ -112,6 +111,7 @@ public class Group3DRender extends BaseRender {
 
         glClearColor(0.5f, 0.5f, 0.5f, 0.5f);
 
+        glEnable(GL_DEPTH_TEST);
 //        glCullFace(GL_BACK);
 //        glEnable(GL_CULL_FACE);
     }
@@ -141,14 +141,14 @@ public class Group3DRender extends BaseRender {
 
         // 刷新vbo数据
         glBindBuffer(GL_ARRAY_BUFFER, vbo[0]);
-        glBufferSubData(GL_ARRAY_BUFFER, 0, MAX_COUNT * 4 * 2,  posBuffer);
+        glBufferSubData(GL_ARRAY_BUFFER, 0, MAX_COUNT * 4 * 3,  posBuffer);
         glBindBuffer(GL_ARRAY_BUFFER, 0);
 
 
         // Use the program object
         glUseProgram(program);
 
-        Matrix.setIdentityM(modelMat, 0);
+//        Matrix.setIdentityM(modelMat, 0);
         Matrix.setIdentityM(viewMat, 0);
         Matrix.setIdentityM(projectionMat, 0);
 
@@ -159,11 +159,13 @@ public class Group3DRender extends BaseRender {
         glUniformMatrix4fv(loc1, 1, false, viewMat, 0);
         int loc2 = glGetUniformLocation(program, "projection");
         glUniformMatrix4fv(loc2, 1, false, projectionMat, 0);
+        Matrix.setIdentityM(modelMat, 0);
         Matrix.translateM(modelMat, 0, 0, 0, 0);
         Matrix.rotateM(modelMat, 0, rx, 0.0f, 1.0f, 0.0f);
         Matrix.rotateM(modelMat, 0, ry, 1.0f, 0.0f, 0.0f);
         int loc3 = glGetUniformLocation(program, "model");
         glUniformMatrix4fv(loc3, 1, false, modelMat, 0);
+
 
         glBindVertexArray(vao[0]);
         glDrawArrays(GL_POINTS, 0, MAX_COUNT);
@@ -175,7 +177,7 @@ public class Group3DRender extends BaseRender {
             vel[i] = r.nextFloat() - 0.5f;
         }
 
-        for (int i = 0; i < pos.length - 1; i+=2) {
+        for (int i = 0; i < pos.length - 2; i+=3) {
             controlSpeed(i);
             Part p = findPart(i);
             p.set.add(i);
@@ -185,23 +187,27 @@ public class Group3DRender extends BaseRender {
     void controlSpeed(int i) {
         float vx = vel[i];
         float vy = vel[i + 1];
-        float vv = (float) Math.sqrt(vx * vx + vy * vy);
+        float vz = vel[i + 2];
+        float v = (float) Math.sqrt(vx * vx + vy * vy + vz * vz);
         float mul = 1f;
-        if (vv >= vmax) {
-            mul = vmax / vv;
-        } else if (vv < vmin) {
-            mul = vmin / vv;
+        if (v >= vmax) {
+            mul = vmax / v;
+        } else if (v < vmin) {
+            mul = vmin / v;
         }
         vel[i] *= mul;
         vel[i + 1] *= mul;
+        vel[i + 2] *= mul;
     }
 
     Part findPart(int posIndex) {
         float x = pos[posIndex];
         float y = pos[posIndex + 1];
+        float z = pos[posIndex + 2];
         int px = (int)x / partW;
         int py = (int)y / partW;
-        int partIndex = px + py * PARTS;
+        int pz = (int)z / partW;
+        int partIndex = px + py * PARTS + pz * PARTS * PARTS;
         Part p = partMap.get(partIndex);
         if (p == null) {
             p = new Part();
@@ -214,14 +220,17 @@ public class Group3DRender extends BaseRender {
     void findNeighborParts(int posIndex) {
         float x = pos[posIndex];
         float y = pos[posIndex + 1];
+        float z = pos[posIndex + 2];
         int px = (int)x / partW;
         int py = (int)y / partW;
+        int pz = (int)z / partW;
         int iNeighbor = 0;
-        for (int i = 0; i < neighborsDir.length - 1; i+=2) {
+        for (int i = 0; i < neighborsDir.length - 2; i+=3) {
             int rx = px + neighborsDir[i];
             int ry = py + neighborsDir[i + 1];
-            if (rx >= 0 && rx < PARTS && ry >= 0 && ry < PARTS) {
-                int partIndex = rx + ry * PARTS;
+            int rz = pz + neighborsDir[i + 2];
+            if (rx >= 0 && rx < PARTS && ry >= 0 && ry < PARTS && rz >= 0 && rz < PARTS) {
+                int partIndex = rx + ry * PARTS + rz * PARTS * PARTS;
                 Part p = partMap.get(partIndex);
                 if (p != null) {
                     for (Integer iPos: p.set) {
@@ -236,13 +245,14 @@ public class Group3DRender extends BaseRender {
     void updateAll() {
         long time = System.currentTimeMillis();
 
-        for (int i = 0; i < pos.length - 1; i+=2) {
+        for (int i = 0; i < pos.length - 2; i+=3) {
 
             findNeighborParts(i);
 
             // 计算群速度：可见的别的点的速度和
             float vxSum = 0;
             float vySum = 0;
+            float vzSum = 0;
             float count = 0;
             for (Integer j : neighbors) {
                 if (j == -1) {
@@ -253,14 +263,17 @@ public class Group3DRender extends BaseRender {
                 }
                 float dx = pos[i] - pos[j];
                 float dy = pos[i + 1] - pos[j + 1];
-                float dis = dx * dx + dy * dy;
+                float dz = pos[i + 2] - pos[j + 2];
+                float dis = dx * dx + dy * dy + dz * dz;
                 if (dis < visiDis) {
                     vxSum += -dx;
                     vySum += -dy;
+                    vzSum += -dz;
                     count++;
                 } else if (dis < closeDis) {
                     vxSum -= dx;
                     vySum -= dy;
+                    vzSum -= dz;
                     count++;
                 }
             }
@@ -269,14 +282,17 @@ public class Group3DRender extends BaseRender {
             if (count > 0) {
                 float vxAve = vxSum / count;
                 float vyAve = vySum / count;
-                vel[i] = vel[i] * 0.8f + vxAve * 0.2f;
+                float vzAve = vzSum / count;
+                vel[i]     = vel[i]     * 0.8f + vxAve * 0.2f;
                 vel[i + 1] = vel[i + 1] * 0.8f + vyAve * 0.2f;
+                vel[i + 2] = vel[i + 2] * 0.8f + vzAve * 0.2f;
                 controlSpeed(i);
             }
 
             // 更新位置，保存到temp里
             float x1 = pos[i] + dt * vel[i];
             float y1 = pos[i + 1] + dt * vel[i + 1];
+            float z1 = pos[i + 2] + dt * vel[i + 2];
             boolean out = false;
             if (x1 < 0 || x1 >= BOUND) {
                 vel[i] = -vel[i];
@@ -286,20 +302,27 @@ public class Group3DRender extends BaseRender {
                 vel[i + 1] = -vel[i + 1];
                 out = true;
             }
-            if (out) {
+            if (z1 < 0 || z1 >= BOUND) {
+                vel[i + 2] = -vel[i + 2];
+                out = true;
+            }
+            if (out) { // 将要出界，那么位置不变
                 tempPos[i] = pos[i];
                 tempPos[i + 1] = pos[i + 1];
+                tempPos[i + 2] = pos[i + 2];
             } else {
                 tempPos[i] = x1;
                 tempPos[i + 1] = y1;
+                tempPos[i + 2] = z1;
             }
         }
 
         // 更新Part，写入新速度
-        for (int i = 0; i < pos.length - 1; i+=2) {
+        for (int i = 0; i < pos.length - 2; i+=3) {
             Part p0 = findPart(i);
             pos[i] = tempPos[i];
             pos[i + 1] = tempPos[i + 1];
+            pos[i + 2] = tempPos[i + 2];
             Part p1 = findPart(i);
             if (p0 == p1) {
                 continue;
