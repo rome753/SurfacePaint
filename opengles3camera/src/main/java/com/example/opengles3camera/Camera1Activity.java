7efec3928cf5271b50cd0018551dee1e930af90e
rome753 <rome753@163.com>
2022-03-19 20:46:54 +0800

    opengles3camera 加入camera1（camera1是yv12格式，camera2是nv12格式）

---
+++ opengles3camera/src/main/java/com/example/opengles3camera/Camera1Activity.java
@@ -0,0 +1,140 @@
+/*
+ * Copyright (C) 2007 The Android Open Source Project
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package com.example.opengles3camera;
+
+import android.app.AlertDialog;
+import android.hardware.Camera;
+import android.hardware.Camera.CameraInfo;
+import android.os.Bundle;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import com.example.opengles3camera.yuv.YUVDetectView;
+
+
+// Need the following import to get access to the app resources, since this
+// class is in a sub-package.
+
+// ----------------------------------------------------------------------
+
+public class Camera1Activity extends AppCompatActivity implements Camera.PreviewCallback {
+    private Camera1Preview mPreview;
+    private YUVDetectView ydv;
+    Camera mCamera;
+    int numberOfCameras;
+    int cameraCurrentlyLocked;
+
+    // The first rear facing camera
+    int defaultCameraId;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_camera1);
+//
+//        // Hide the window title.
+//        requestWindowFeature(Window.FEATURE_NO_TITLE);
+//        getWindow().addFlags(WindowManager.LayoutParams.FLAG_FULLSCREEN);
+
+        // Create a RelativeLayout container that will hold a SurfaceView,
+        // and set it as the content of our activity.
+        mPreview = findViewById(R.id.surface);
+        ydv = findViewById(R.id.ydv);
+
+        // Find the total number of cameras available
+        numberOfCameras = Camera.getNumberOfCameras();
+
+        // Find the ID of the default camera
+        CameraInfo cameraInfo = new CameraInfo();
+            for (int i = 0; i < numberOfCameras; i++) {
+                Camera.getCameraInfo(i, cameraInfo);
+                if (cameraInfo.facing == CameraInfo.CAMERA_FACING_BACK) {
+                    defaultCameraId = i;
+                }
+            }
+
+    }
+
+    @Override
+    protected void onResume() {
+        super.onResume();
+
+        // Open the default i.e. the first rear facing camera.
+        mCamera = Camera.open();
+        mCamera.setPreviewCallback(this);
+        mCamera.setDisplayOrientation(90);
+        cameraCurrentlyLocked = defaultCameraId;
+        mPreview.setCamera(mCamera);
+    }
+
+    @Override
+    protected void onPause() {
+        super.onPause();
+
+        // Because the Camera object is a shared resource, it's very
+        // important to release it when the activity is paused.
+        if (mCamera != null) {
+            mPreview.setCamera(null);
+            mCamera.setPreviewCallback(null);
+            mCamera.release();
+            mCamera = null;
+        }
+    }
+
+    private void switchCam() {
+        if (numberOfCameras == 1) {
+            AlertDialog.Builder builder = new AlertDialog.Builder(this);
+            builder.setMessage("Device has only one camera!")
+                   .setNeutralButton("Close", null);
+            AlertDialog alert = builder.create();
+            alert.show();
+            return;
+        }
+
+        // OK, we have multiple cameras.
+        // Release this camera -> cameraCurrentlyLocked
+        if (mCamera != null) {
+            mCamera.stopPreview();
+            mPreview.setCamera(null);
+            mCamera.setPreviewCallback(null);
+            mCamera.release();
+            mCamera = null;
+        }
+
+        // Acquire the next camera and request Preview to reconfigure
+        // parameters.
+        mCamera = Camera
+                .open((cameraCurrentlyLocked + 1) % numberOfCameras);
+        cameraCurrentlyLocked = (cameraCurrentlyLocked + 1)
+                % numberOfCameras;
+        mPreview.switchCamera(mCamera);
+
+        // Start the preview
+        mCamera.startPreview();
+    }
+
+    @Override
+    public void onPreviewFrame(byte[] data, Camera camera) {
+        if(camera == null) return;
+        Camera.Size size = camera.getParameters().getPreviewSize(); //获取预览大小
+        final int w = size.width;
+        final int h = size.height;
+        ydv.inputAsync(data, w, h);
+    }
+}
+
+// ----------------------------------------------------------------------
